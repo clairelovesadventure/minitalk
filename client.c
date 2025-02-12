7fb68555fe000d705268872933a3c82a8b0e6067
Shuxin Tan <shuxintan@Shuxins-MacBook-Pro.local>
2025-02-12 16:58:51 +0100

    change

--- client.c
+++ client.c
@@ -2,61 +2,86 @@
 #include <signal.h>
 #include <unistd.h>
 
-static int g_signal_received = 0;
-
-// 处理服务器的确认信号
-static void signal_handler(int sig)
+typedef struct s_client_data
 {
+    int signal_received;
+} t_client_data;
+
+static void signal_handler(int sig, siginfo_t *info, void *context)
+{
+    t_client_data *data;
+
+    (void)info;
+    (void)context;
     (void)sig;
-    g_signal_received = 1;
+    data = (t_client_data *)context;
+    data->signal_received = 1;
 }
 
-// 发送一个字节的数据
-static void	send_byte(int server_pid, unsigned char byte)
+static void send_bit(int server_pid, int bit, t_client_data *data)
 {
-    int	bit_position;
+    data->signal_received = 0;
+    if (bit)
+        kill(server_pid, SIGUSR1);
+    else
+        kill(server_pid, SIGUSR2);
+    while (!data->signal_received)
+        usleep(100);
+}
+
+static void send_byte(int server_pid, unsigned char byte, t_client_data *data)
+{
+    int bit_position;
 
     bit_position = 7;
     while (bit_position >= 0)
     {
-        g_signal_received = 0;
-        if (byte & (1 << bit_position))
-            kill(server_pid, SIGUSR1);
-        else
-            kill(server_pid, SIGUSR2);
-        
-        // 等待服务器确认
-        while (!g_signal_received)
-            usleep(100);
+        send_bit(server_pid, byte & (1 << bit_position), data);
         bit_position--;
     }
 }
 
-int	main(int argc, char **argv)
+static int init_client(t_client_data *data)
 {
-    int		server_pid;
-    char	*msg;
-    int		i;
+    struct sigaction sa;
+
+    data->signal_received = 0;
+    sa.sa_sigaction = signal_handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+        return (-1);
+    return (0);
+}
+
+static void send_message(int server_pid, char *msg, t_client_data *data)
+{
+    int i;
+
+    i = 0;
+    while (msg[i])
+        send_byte(server_pid, msg[i++], data);
+    send_byte(server_pid, '\0', data);
+}
+
+int main(int argc, char **argv)
+{
+    t_client_data data;
+    int server_pid;
 
     if (argc != 3)
     {
         ft_printf("Usage: ./client <server_pid> <message>\n");
         return (1);
     }
-    
-    // 设置信号处理函数
-    signal(SIGUSR1, signal_handler);
-    
     server_pid = ft_atoi(argv[1]);
     if (server_pid <= 0)
     {
         ft_printf("Error: invalid PID\n");
         return (1);
     }
-    msg = argv[2];
-    i = 0;
-    while (msg[i])
-        send_byte(server_pid, msg[i++]);
-    send_byte(server_pid, '\0');  // 发送终止符
+    if (init_client(&data) == -1)
+        return (1);
+    send_message(server_pid, argv[2], &data);
     return (0);
 } 
